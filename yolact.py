8f04249dcbe677a183a7c7f820c1708e4a9be4fb
Chi Wai Lau <clau@apple.com>
2021-03-03 22:49:54 -0800

    Update yolact.py

--- yolact.py
+++ yolact.py
@@ -1255,8 +1255,8 @@ class Yolact(nn.Module):
             logger.warning("Some parameters in the checkpoint have a different shape in the current model, "
                            "and are initialized as they should be: {}".format(", ".join(keys_mismatch)))
 
-        if args.coco_transfer or args.yolact_transfer:
-            logger.warning("`--coco_transfer` or `--yolact_transfer` is no longer needed. The code will automatically detect and convert YOLACT-trained weights now.")
+        # if args.coco_transfer or args.yolact_transfer:
+            # logger.warning("`--coco_transfer` or `--yolact_transfer` is no longer needed. The code will automatically detect and convert YOLACT-trained weights now.")
 
         self.load_state_dict(state_dict)
 
